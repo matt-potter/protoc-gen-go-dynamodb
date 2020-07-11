85a6a93949c7e2b54fe57631c21ac22130e058c8
Matt Potter <me@mattpotter.co.uk>
2020-07-11 22:28:24 +0200

    template changes

--- generator/generator.go
+++ generator/generator.go
@@ -3,11 +3,15 @@ package generator
 import (
 	"errors"
 	"fmt"
+	"html/template"
 	"log"
 	"os"
 	"path"
+	"strings"
 
+	"github.com/jinzhu/inflection"
 	"github.com/matt-potter/protoc-gen-go-dynamodb/dynamopb"
+	"github.com/matt-potter/protoc-gen-go-dynamodb/templates"
 
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/proto"
@@ -21,12 +25,20 @@ type Generator struct {
 	pathType    string
 	plugin      *protogen.Plugin
 	importMap   map[string]string
+	funcMap     template.FuncMap
 }
 
 // NewGenerator is a wrapper function to generate our dynamo code
 func NewGenerator(plugin *protogen.Plugin, pathType string) (*Generator, error) {
 
 	gen := &Generator{}
+
+	gen.funcMap = make(template.FuncMap)
+
+	gen.funcMap["ToLower"] = strings.ToLower
+	gen.funcMap["ToTitle"] = strings.ToTitle
+	gen.funcMap["Singular"] = inflection.Singular
+	gen.funcMap["Plural"] = inflection.Plural
 
 	gen.pathType = pathType
 
@@ -73,8 +85,6 @@ func (g *Generator) Generate() {
 
 	g.generateHeaderAndClient()
 
-	g.generateMethods()
-
 	for _, pfile := range g.plugin.Files {
 
 		if !pfile.Generate {
@@ -84,6 +94,16 @@ func (g *Generator) Generate() {
 		if len(pfile.Messages) == 0 {
 			continue
 		}
+
+		if g.hasStorableMessages(pfile) == false {
+			continue
+		}
+
+		filename := pfile.GeneratedFilenamePrefix + ".db.go"
+
+		f := g.plugin.NewGeneratedFile(filename, pfile.GoImportPath)
+
+		g.generateMethods(pfile, f)
 
 		// file := g.plugin.NewGeneratedFile(fmt.Sprintf("%s.db.go", pfile.GeneratedFilenamePrefix), pfile.GoImportPath)
 
@@ -218,62 +238,79 @@ func getImportPath(files []*protogen.File) (string, error) {
 
 func (g *Generator) generateHeaderAndClient() {
 
+	var f *protogen.GeneratedFile
+
 	switch g.pathType {
-	case "IMPORT":
-		f := g.plugin.NewGeneratedFile(fmt.Sprintf("%s/%s.db.go", g.protoPath, g.packageName), protogen.GoImportPath(g.importPath))
-		f.P(fmt.Sprintf("package %s", g.packageName))
+
 	case "SOURCE_RELATIVE":
-		f := g.plugin.NewGeneratedFile(fmt.Sprintf("%s/%s.db.go", g.importPath, g.packageName), protogen.GoImportPath(g.importPath))
-		f.P(fmt.Sprintf("package %s", g.packageName))
+		f = g.plugin.NewGeneratedFile(fmt.Sprintf("%s/%s.db.go", g.protoPath, g.packageName), protogen.GoImportPath(g.importPath))
 	default:
-		f := g.plugin.NewGeneratedFile(fmt.Sprintf("%s/%s.db.go", g.importPath, g.packageName), protogen.GoImportPath(g.importPath))
-		f.P(fmt.Sprintf("package %s", g.packageName))
+		f = g.plugin.NewGeneratedFile(fmt.Sprintf("%s/%s.db.go", g.importPath, g.packageName), protogen.GoImportPath(g.importPath))
+
 	}
+
+	t, err := template.New("master").Funcs(g.funcMap).Parse(templates.DynamoDBHeaderAndClient)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := struct {
+		PackageName string
+	}{
+		PackageName: g.packageName,
+	}
+
+	t.Execute(f, config)
 
 }
 
-func (g *Generator) generateMethods() {
+func (g *Generator) generateMethods(file *protogen.File, f *protogen.GeneratedFile) {
 
-	for _, file := range g.plugin.Files {
+	for _, msg := range file.Messages {
 
-		if !file.Generate {
+		if proto.HasExtension(msg.Desc.Options(), dynamopb.E_Storable) {
+			storable := proto.GetExtension(msg.Desc.Options(), dynamopb.E_Storable).(bool)
+			if !storable {
+				continue
+			}
+		} else {
 			continue
 		}
 
-		if len(file.Messages) == 0 {
-			continue
+		var pk *dynamopb.Pk
+		var gsis []*dynamopb.Gsi
+
+		if proto.HasExtension(msg.Desc.Options(), dynamopb.E_PrimaryIndex) {
+			pk = proto.GetExtension(msg.Desc.Options(), dynamopb.E_PrimaryIndex).(*dynamopb.Pk)
 		}
 
-		for _, msg := range file.Messages {
-
-			if proto.HasExtension(msg.Desc.Options(), dynamopb.E_Storable) {
-				if !proto.GetExtension(msg.Desc.Options(), dynamopb.E_Storable).(bool) {
-					continue
-				}
-			}
-
-			var pk *dynamopb.Pk
-			var gsis []*dynamopb.Gsi
-
-			if proto.HasExtension(msg.Desc.Options(), dynamopb.E_PrimaryIndex) {
-				pk = proto.GetExtension(msg.Desc.Options(), dynamopb.E_PrimaryIndex).(*dynamopb.Pk)
-			}
-
-			if proto.HasExtension(msg.Desc.Options(), dynamopb.E_GlobalSecondaryIndexes) {
-				gsis = proto.GetExtension(msg.Desc.Options(), dynamopb.E_GlobalSecondaryIndexes).([]*dynamopb.Gsi)
-			}
-
-			if pk == nil {
-				log.Fatalf("storable is set but no primary_key is defined in %s", file.Desc.Path())
-			}
-
-			f := g.plugin.NewGeneratedFile(fmt.Sprintf("%s.db.go", file.GeneratedFilenamePrefix), file.GoImportPath)
-
-			log.Println(pk, gsis)
-
-			g.generateBaseActions(f)
-
+		if proto.HasExtension(msg.Desc.Options(), dynamopb.E_GlobalSecondaryIndexes) {
+			gsis = proto.GetExtension(msg.Desc.Options(), dynamopb.E_GlobalSecondaryIndexes).([]*dynamopb.Gsi)
 		}
+
+		if pk == nil {
+			log.Fatalf("storable is set but no primary_key is defined in %s", file.Desc.Path())
+		}
+
+		t, err := template.New("master").Funcs(g.funcMap).Parse(templates.DynamoDBMessageHeaders)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		config := struct {
+			PackageName  string
+			MessageName  string
+			PrimaryIndex *dynamopb.Pk
+			GSIs         []*dynamopb.Gsi
+		}{
+			PackageName:  string(file.GoPackageName),
+			MessageName:  msg.GoIdent.GoName,
+			PrimaryIndex: pk,
+			GSIs:         gsis,
+		}
+
+		t.Execute(f, config)
 
 	}
 
@@ -314,6 +351,25 @@ func (g *Generator) generateMethods() {
 	// f.P(`}`)
 }
 
+func (g *Generator) hasStorableMessages(f *protogen.File) bool {
+
+	hasStorable := false
+
+	for _, msg := range f.Messages {
+		if proto.HasExtension(msg.Desc.Options(), dynamopb.E_Storable) {
+			storable := proto.GetExtension(msg.Desc.Options(), dynamopb.E_Storable).(bool)
+			if !storable {
+				continue
+			}
+			hasStorable = true
+		} else {
+			continue
+		}
+	}
+
+	return hasStorable
+}
+
 func (g *Generator) generateBaseActions(f *protogen.GeneratedFile) {
-	f.P(`package `, g.packageName)
+
 }
--- templates/dynamodb.go
+++ templates/dynamodb.go
@@ -1,8 +1,42 @@
 package templates
 
-// DynamoDBCreateFunc is the template for the create func
-var DynamoDBCreateFunc string = `
-func (d *db) Create(ctx context.Context, item *api.Activity) (*api.Activity, error) {
+var DynamoDBHeaderAndClient string = `
+package {{ .PackageName }}
+
+import "github.com/aws/aws-sdk-go/service/dynamodb"
+
+type DB struct {
+	Client *dynamodb.DynamoDB
+}
+
+func NewDB(ddb *dynamodb.DynamoDB) *DB {
+	return &DB{
+		Client: ddb,
+	}
+}
+`
+
+var DynamoDBMessageHeaders string = `
+package {{ .PackageName }}
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"log"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+	"github.com/pkg/errors"
+)
+
+const TableName{{ .MessageName }} = "{{ .MessageName }}"
+`
+
+var DynamoDBCreateMessage string = `
+func (d *DB) DDBCreate{{ .MessageName }}(ctx context.Context, item *{{ .MessageName }}) (*{{ .MessageName }}, error) {
 
 	av, err := dynamodbattribute.MarshalMap(item)
 	if err != nil {
@@ -16,9 +50,11 @@ func (d *db) Create(ctx context.Context, item *api.Activity) (*api.Activity, err
 	}
 
 	input := &dynamodb.PutItemInput{
-		Item:                av,
-		TableName:           aws.String(TableNameActivity),
-		ConditionExpression: expr.Condition(),
+		Item:                      av,
+		TableName:                 aws.String(TableName{{ .MessageName }}),
+		ConditionExpression:       expr.Condition(),
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
 	}
 
 	_, err = d.Client.PutItemWithContext(ctx, input)
@@ -29,5 +65,186 @@ func (d *db) Create(ctx context.Context, item *api.Activity) (*api.Activity, err
 
 	return item, nil
 
+}
+`
+
+var DynamoDBGetMessageNoHash = `
+func (d *DB) DDBGet{{ .MessageName }}(ctx context.Context, name string) (*{{ .MessageName }}, error) {
+
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(name),
+			},
+		},
+		TableName: aws.String(TableName{{ .MessageName }}),
+	}
+
+	res, err := d.Client.GetItemWithContext(ctx, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	{{ .MessageName | ToLower }} := &{{ .MessageName }}{}
+
+	err = dynamodbattribute.UnmarshalMap(res.Item, activity)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if {{ .MessageName | ToLower }}.Name == "" {
+		return nil, errors.New("Not found")
+	}
+
+	return {{ .MessageName | ToLower }}, nil
+}`
+
+var DynamoDBGetMessageWithHash = `
+func (d *DB) DDBGet{{ .MessageName }}(ctx context.Context, name string) (*{{ .MessageName }}, error) {
+
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				S: aws.String(name),
+			},
+		},
+		TableName: aws.String(TableName{{ .MessageName }}),
+	}
+
+	res, err := d.Client.GetItemWithContext(ctx, input)
+
+	if err != nil {
+		return nil, err
+	}
+
+	{{ .MessageName | ToLower }} := &{{ .MessageName }}{}
+
+	err = dynamodbattribute.UnmarshalMap(res.Item, activity)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if {{ .MessageName | ToLower }}.Name == "" {
+		return nil, errors.New("Not found")
+	}
+
+	return {{ .MessageName | ToLower }}, nil
+}`
+
+var DynamoDBGetMessageMulti = `
+func (d *DB) DDBGet{{ .MessageName | Plural }}(ctx context.Context, prefix string, startKey string, limit int64) ({{ .MessageName | Plural | ToLower }} []*{{ .MessageName }}, lastEvaluatedKey string, err error) {
+
+	type lastKeyResponse map[string]struct {
+	}
+
+	expr, err := expression.NewBuilder().WithKeyCondition(expression.KeyBeginsWith(expression.Key("collection"), prefix)).Build()
+
+	if err != nil {
+		return nil, "", err
+	}
+
+	input := &dynamodb.QueryInput{
+		KeyConditionExpression:    expr.KeyCondition(),
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		Limit:                     aws.Int64(limit),
+		TableName:                 aws.String(TableName{{ .MessageName }}),
+		IndexName:                 aws.String("collection-id-index"),
+	}
+
+	if startKey != "" {
+
+		decoded, err := base64.StdEncoding.DecodeString(startKey)
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		log.Println("Decoded", string(decoded))
+
+		exclusiveStartKey, err := dynamodbattribute.MarshalMap(string(decoded))
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		input.ExclusiveStartKey = exclusiveStartKey
+
+		log.Println("EX:", exclusiveStartKey)
+	}
+
+	res, err := d.Client.QueryWithContext(ctx, input)
+
+	if err != nil {
+		return nil, "", err
+	}
+
+	lastEvaluatedKey = ""
+
+	if len(res.LastEvaluatedKey) > 0 {
+
+		log.Println(res.LastEvaluatedKey)
+
+		pageResponseMap := make(map[string]interface{})
+
+		for _, val := range res.LastEvaluatedKey {
+			log.Println(val.GoString())
+		}
+
+		err = dynamodbattribute.UnmarshalMap(res.LastEvaluatedKey, &pageResponseMap)
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		b, err := json.Marshal(pageResponseMap)
+
+		if err != nil {
+			return nil, "", err
+		}
+
+		lastEvaluatedKey = base64.StdEncoding.EncodeToString(b)
+	}
+
+	err = dynamodbattribute.UnmarshalListOfMaps(res.Items, &{{ .MessageName | Plural | ToLower }})
+
+	if err != nil {
+		return nil, "", err
+	}
+
+	log.Println("LAST KEY", lastEvaluatedKey)
+
+	return activities, lastEvaluatedKey, nil
+}
+`
+var DynamoDBUpdateMessage = `
+func (d *DB) DDBUpdate{{ .MessageName }}(ctx context.Context, item *{{ .MessageName }}) (*{{ .MessageName }}, error) {
+
+	_, err := d.DDBGet{{ .MessageName }}(ctx, item.Name)
+
+	if err != nil {
+		return item, err
+	}
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		return item, err
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(TableName{{ .MessageName }}),
+	}
+
+	_, err = d.Client.PutItemWithContext(ctx, input)
+
+	if err != nil {
+		return item, err
+	}
+
+	return item, nil
 }
 `
